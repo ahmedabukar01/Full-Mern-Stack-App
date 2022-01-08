3b5928912b2dc86210270033fd61c091e95330d3
ahmed <ahmedlearn01@gmail.com>
2022-01-08 10:00:57 -0800

    writePost

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import Home from "./pages/home/Home";
 import Single from "./pages/single/Single";
+import Write from "./pages/write/Write";
 import Topbar from "./TopBar/Topbar";
 
 function App() {
   return (
     <>
     <Topbar/>
-    <Single />
+    <Write />
     </>
   );
 }
---
+++ src/pages/write/Write.jsx
@@ -0,0 +1,29 @@
+import './write.css';
+import inputImg from '../assets/setup2.jpg';
+
+const Write = () => {
+    return (
+        <div className='write'>
+            <img src={inputImg} alt="" className='inputImg'/>
+            <form className="writeForm">
+                <div className="writeFormGroup">
+                    <label htmlFor="fileInput">
+                    <i className="writeIcon fas fa-plus"></i>
+                    </label>
+                    <input type="file" id="fileInput" style={{display: 'none'}} />
+                    <input type="text" placeholder='Title' className="writeInput" autoFocus={true} />
+                </div>
+                <div className="writeFormGroup">
+                    <textarea
+                     placeholder='Tell your story...'
+                     type="text"
+                     className="writeInput WriteText"
+                     ></textarea>
+                </div>
+                <button className="writeSubmit">Publish</button>
+            </form>
+        </div>
+    )
+}
+
+export default Write
